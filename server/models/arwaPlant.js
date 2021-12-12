3b8ada2c6dc84aca073a151707657ec095637395
Nikhil Kumar <nknapster@gmail.com>
2021-12-12 18:40:42 +0530

    date +5.5hrs

--- server/models/arwaPlant.js
+++ server/models/arwaPlant.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const arwaPlantSchema = new mongoose.Schema({
   millState: String, //on off mantainance
-  logTime: {type:Date,default:Date.now},
+  logTime: {type:Date,default:+new Date() + 5.5*60*60*1000},
   naration: String,
   user:String
 })
