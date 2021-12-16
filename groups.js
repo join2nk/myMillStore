1d253f41bfe02fd765cc9dfc447cd90b2ec38035
Nikhil Kumar <nknapster@gmail.com>
2021-12-16 18:52:10 +0530

    new model design notes

---
+++ groups.js
@@ -0,0 +1,66 @@
+/**
+
+-gatepass no
+-date
+-broker [{}]
+-party [{party name address bank ac details photo}]
+-truck no []&non
+-hemal []
+-in time
+-out time
+-state [
+  in by user at time,
+  verified by user at time,
+  loading/unloading by user at time,
+  gatepass by user at time,
+  out by user at time
+]
+-gatepassGroup- in out sate/fci/nonTradeOut purc/dmo/nonTradeIn   
+-sauda:  sauda no
+
+-billed -- items & rateCut & bardana & cgss 
+--quantity bags qt others
+--naration
+--rate
+-
+items & rateCut & bardana & cgss 
+quantity bags qt others
+--naration
+--rate
+-other income and expenses
+-naration
+-hemal
+
+
+
+
+
+sauda
+-sauda no
+-type - sale purchase DO
+-date
+-broker
+-party
+-quantity 
+-quantity type
+-rate
+-dilivary condition
+-notes []
+func pending()
+
+
+
+
+
+
+payement and recites
+-date
+-type payemtn reciete journal
+-from
+-to/by
+-against [{amoutn,gatepass}]
+-naration
+
+
+
+*/
